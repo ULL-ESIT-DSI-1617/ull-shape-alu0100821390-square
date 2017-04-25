33d74a20168c9a89a08cab8c7c70661f167bfde2
alu0100821390 <alu0100821390@ull.edu.es>
2017-04-25 14:18:11 +0100

    Añadir pruebas

---
+++ test/test.js
@@ -0,0 +1,9 @@
+let Square = require('../lib/square');
+
+describe ("getArea Square", function() {
+	it ("must compute the square area correctly", function() {
+		let s = new Square({ width: 100 });
+		let result = s.getArea();
+		result.should.equal(10000);
+	})
+});
